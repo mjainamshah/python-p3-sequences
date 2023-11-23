0e3efba006a2295216da3b6e0af2aed04995c3ed
mjainamshah <jainam.shah@student.moringaschool.com>
2023-11-23 11:44:53 +0300

    COMPLETED

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])  # Empty list for when the length 0
+    elif length == 1:
+        print([0])  # List with a single element '0' when the length is 1
+    else:
+        fibonacci_sequence = [0, 1]  # Initializes the sequence with the first two elements
+        while len(fibonacci_sequence) < length:
+            next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_number)
+        print(fibonacci_sequence)
